725387a887975eaccd1defcd403385f6500cef1b
carlo <carloseduardofernandes446@gmail.com>
2026-05-25 00:07:27 -0300

    Salva progresso do dia na minha branch

--- app/src/main/java/com/example/bibliounifornew/adm/Telarf32LivrosCrudADM.kt
+++ app/src/main/java/com/example/bibliounifornew/adm/Telarf32LivrosCrudADM.kt
@@ -4,11 +4,24 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.bibliounifornew.R
+import com.google.android.material.button.MaterialButton
 
 class Telarf32LivrosCrudADM : Fragment(R.layout.telarf32_livros_crud_adm) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Quando precisar linkar botões ou textos desta tela, coloque os findViewById aqui
+
+        // 1. Linkando o botão do XML com o Kotlin
+        val btnEditarInformacoes = view.findViewById<MaterialButton>(R.id.btnEditarInformacoes)
+
+        // 2. Ação de clique
+        btnEditarInformacoes.setOnClickListener {
+
+            // Navegando para a TelaRF37EditarMidia
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.frameLayout, TelaRF37EditarMidia())
+                .addToBackStack(null) // Salva o histórico para o botão "Voltar" do celular funcionar
+                .commit()
+        }
     }
 }
